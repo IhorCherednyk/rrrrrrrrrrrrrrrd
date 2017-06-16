12d4596a99fcffacc9d8c26ad494f3a5b7a5715f
Ihor <ihor_cherednyk@mail.ru>
2017-06-16 08:11:23 +0300

    aassdd

---
+++ controllers/BobController.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use app\modules\setting\helpers\SettingHelper;
+
+
+/**
+ * Description of BobController
+ *
+ * @author Anastasiya
+ */
+class BobController extends FrontControlller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
