4df23822b1515fae210671642e50b0e3f326c126
Ihor <ihor_cherednyk@mail.ru>
2017-09-05 07:53:24 +0300

    bob wowrk

--- modules/streams/views/stream/index.php
+++ modules/streams/views/stream/index.php
@@ -6,12 +6,11 @@ use yii\helpers\Url;
 <?php $this->title = 'Стримы'?>
 <?php if (count($data)): ?>
     
-    <section class="main-section streams">\
-            
+    <section class="main-section streams">
         <div class="row">
             <?php foreach ($data as $stream) : ?>
                 <div class="col-md-6">
-                    <a href="<?= Url::to(['/video/index/stream', 'name' => $stream->channel->name, 'id' => $stream->channel->_id]) ?>" class="box-stream-wrapper" style="background-image: url(<?= $stream->preview->medium ?>)">
+                    <a href="<?= Url::to(['/streams/stream/view-stream', 'name' => $stream->channel->name, 'id' => $stream->channel->_id]) ?>" class="box-stream-wrapper" style="background-image: url(<?= $stream->preview->medium ?>)">
                         <div class="box-stream-overlay">
                             <span class="box-stream-content">
                                 <span class="channel">
@@ -23,8 +22,8 @@ use yii\helpers\Url;
                                 <span class="viewers"><i class="fa fa-eye fa-fw"></i><?= $stream->viewers ?></span>
                             </span>
                             <span class="twitch-logo">
-                                        <?= Html::img($stream->channel->logo) ?>
-                                    </span>
+                                <?= Html::img($stream->channel->logo) ?>
+                            </span>
                         </div>
                     </a>
                 </div>
---
+++ modules/streams/views/stream/view-stream.php
@@ -0,0 +1,6 @@
+<?php $this->title = $name ?>
+<section class="main-section">
+    <div class="stream-wrapper">
+        <iframe src="https://player.twitch.tv/?channel=<?= $name?>" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
+    </div>
+</section>
